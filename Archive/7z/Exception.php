28d3ca2ce31d75c43b039031b9b722eb62de1977
Gemorroj <wapinet@gmail.com>
2012-11-11 00:06:48 +0300

    fix link

--- Archive/7z/Exception.php
+++ Archive/7z/Exception.php
@@ -6,7 +6,7 @@
  * @author    Gemorroj
  * @copyright 2012 http://wapinet.ru
  * @license   http://www.gnu.org/licenses/gpl-3.0.txt
- * @link      http://wapinet.ru/gmanager/
+ * @link      https://github.com/Gemorroj/Archive_7z
  * @version   0.1 alpha
  *
  */
