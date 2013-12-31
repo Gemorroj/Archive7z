79040359f662e074d096f83d42fe5b6c20e20a23
Gemorroj <wapinet@gmail.com>
2013-12-31 18:37:33 +0200

    upd tests loader

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,6 +1,2 @@
 <?php
-$baseDir = dirname(__DIR__);
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-$loader->add('Archive7z', array($baseDir.'/src/', $baseDir.'/Tests/'));
-$loader->register();
+require __DIR__.'/../vendor/autoload.php';
