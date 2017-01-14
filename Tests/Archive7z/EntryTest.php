1fe72389f2a43049ea822b8b7fc6d8232e2a82c2
Gemorroj <wapinet@gmail.com>
2017-01-14 11:11:31 +0300

    change license from gpl 3.0 to lgpl 3.0

--- Tests/Archive7z/EntryTest.php
+++ Tests/Archive7z/EntryTest.php
@@ -1,10 +1,41 @@
 <?php
 namespace Tests\Archive7z;
 
+use Archive7z\Archive7z;
+
+//TODO
 class EntryTest extends \PHPUnit_Framework_TestCase
 {
-    public function test()
+    protected $cliPath;
+    protected $fixturesDir;
+    protected $baseDir;
+
+    protected function setUp()
     {
-        // todo
+        $this->baseDir = dirname(__DIR__);
+        $this->fixturesDir = $this->baseDir . DIRECTORY_SEPARATOR . 'fixtures';
+    }
+
+
+    /**
+     * @see https://github.com/Gemorroj/Archive7z/issues/5
+     */
+    public function testPackedSize()
+    {
+        $expectedResults = array(
+            '60572',
+            '104822',
+            '', // второй файл в solid блоке
+            '19',
+            '0' // directory
+        );
+        $actualResults = array();
+
+        $archive = new Archive7z($this->fixturesDir . '/test.7z', $this->cliPath);
+        foreach ($archive->getEntries() as $entry) {
+            $actualResults[] = $entry->getPackedSize();
+        }
+
+        self::assertEquals($expectedResults, $actualResults);
     }
 }
--- src/Archive7z/Archive7z.php
+++ src/Archive7z/Archive7z.php
@@ -1,13 +1,4 @@
 <?php
-/**
- *
- * This software is distributed under the GNU GPL v3.0 license.
- *
- * @author    Gemorroj
- * @copyright 2013 http://wapinet.ru
- * @license   http://www.gnu.org/licenses/gpl-3.0.txt
- * @link      https://github.com/Gemorroj/Archive7z
- */
 
 namespace Archive7z;
 
