87c1976b1449d6b108da752066cacfd8b3b69ba0
Gemorroj <wapinet@mail.ru>
2025-12-06 18:05:20 +0300

    build: drop php 8.0 support

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,8 +8,8 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        '@PHP80Migration' => true,
-        '@PHP80Migration:risky' => true,
+        '@PHP8x1Migration' => true,
+        '@PHP8x1Migration:risky' => true,
 
         'combine_consecutive_issets' => true,
         'combine_consecutive_unsets' => true,
